06430d6f91d7ae946d79b00eb4884dbe7aacf9e0
rohansharma06 <rohansharma251998@gmail.com>
2020-05-30 13:10:50 +0530

    add delete all module

--- controllers/deleteTask_controller.js
+++ controllers/deleteTask_controller.js
@@ -1,4 +1,5 @@
 const ToDo=require('../models/todoList');
+//---- deleting delected task
 module.exports.delete=function(req,res){
     let id=req.query.id;
 
@@ -10,4 +11,21 @@ module.exports.delete=function(req,res){
         console.log(req.query);
         return res.redirect('back');
     });
+}
+
+//---- deleting all task
+module.exports.all=function(req,res){
+    ToDo.find({},function(err,list){
+        for(let i of list){
+            ToDo.findByIdAndDelete(i._id,function(err){
+                if(err){
+                    console.log("Error in deleting a task");
+                    return;
+                }
+                // console.log(req.query);
+                // return res.redirect('/');
+            });
+        }
+    });
+    return res.redirect('/');
 }
