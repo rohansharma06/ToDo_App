df9a878c8341adeb6e2c65c34bb0a058f1ab20c0
rohansharma06 <rohansharma251998@gmail.com>
2020-05-30 13:03:38 +0530

    update some style

--- assets/js/home.js
+++ assets/js/home.js
@@ -14,3 +14,23 @@ document.getElementById("year").innerHTML=year;
 var days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 var day=today.getDay();
 document.getElementById("currDay").innerHTML=days[day-1];
+
+//---- set value and min property of dueDate input
+let mydueDate;
+if(today.getMonth()+1 >=9){
+    mydueDate=today.getFullYear()+"-"+("0"+(today.getMonth()+1))+"-"+today.getDate();
+}
+else{
+    mydueDate=today.getFullYear()+"-"+("0"+(today.getMonth()+1))+"-"+today.getDate();
+}
+var find=document.querySelector('input[type="date"]');
+find.value=mydueDate;
+find.min=mydueDate;
+
+// ---- changing 
+// function cate(category){
+//     console.log(category);
+//     return;
+// }
+// var cate=document.getElementsByClassName("listItems").id;
+// console.log(cate);
